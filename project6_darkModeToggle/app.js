b16d1d095a4e02790033c8812e6979f8e586d682
ShantiMishra <shantimishra2006@gmail.com>
2025-11-28 10:56:35 +0530

    Dark mode toggle

--- project6_darkModeToggle/app.js
+++ project6_darkModeToggle/app.js
@@ -1,24 +1,24 @@
 const inputEl = document.querySelector(".input");
-
 const bodyEl = document.querySelector("body");
 
 inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 
 updateBody();
 
-function updateBody() {
-  if (inputEl.checked) {
-    bodyEl.style.background = "black";
-  } else {
-    bodyEl.style.background = "white";
+function updateBody(){
+  if(inputEl.checked){
+    bodyEl.style.backgroundColor="black";
+  }
+  else{
+    bodyEl.style.backgroundColor="white";
   }
 }
 
 inputEl.addEventListener("input", () => {
   updateBody();
   updateLocalStorage();
-});
+})
 
-function updateLocalStorage() {
-  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+function updateLocalStorage(){
+    localStorage.setItem("mode", JSON.stringify(inputEl.checked));
 }
