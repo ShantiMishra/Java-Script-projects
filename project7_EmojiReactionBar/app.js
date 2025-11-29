8401d7f97a74bc38651bfcc85fe52e12e6bc8c04
ShantiMishra <shantimishra2006@gmail.com>
2025-11-29 11:09:02 +0530

    Emoji Reaction Bar

---
+++ project7_EmojiReactionBar/app.js
@@ -0,0 +1,5 @@
+const reactionEl = document.querySelectorAll(".reaction");
+const statusEl = document.getElementById("status");
+const totalEl = document.getElementById("total");
+
+const counts = [0,0,0,0,0];
